ff84ebeb8ce2552fffa1ba384da5d22d885575a0
veiii <11veigar@gmail.com>
2017-02-17 18:58:49 +0100

    Update sql.php

--- config/sql.php
+++ config/sql.php
@@ -1,19 +1,8 @@
 <?php
+
 $host='localhost';
 $dbase='QuoteYourNote';
 $user='root';
 $pass='';
-/*
-kod sql to tworzenia tablic
-
-CREATE TABLE articles(
-id integer auto_increment,
-title varchar(100),
-content text,
-date_add datetime,
-autor varchar(100),
-primary key(id)
-);
-*/
 
 ?>
